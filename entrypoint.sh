a7f4535aefa7a93679ceb3a4425124e9a24dd128
jipp <wolfgang.keller@wobilix.de>
2021-01-26 13:58:29 +0100

    modified:   entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+FILE=/etc/os-release
+
+if [ -f "$FILE" ]; then
+    source /etc/os-release
+    echo $ID $VERSION_ID
+    echo
+fi
+
 if [ $# -eq 0 ]; then
     cat help.txt
 fi
