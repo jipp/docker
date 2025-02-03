a769b62d22e1782ead06a8a0e0843d088e003291
jipp <wolfgang.keller@wobilix.de>
2025-02-03 16:47:24 +0100

    moved to bash

--- lemonpi/scripts/minecraft.sh
+++ lemonpi/scripts/minecraft.sh
@@ -1,16 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-STATUS=`/usr/bin/docker inspect --format='{{.State.Health.Status}}' minecraft`
+Health () {
+	echo Server: $1
 
-echo minecraft server status: $STATUS
+	STATUS=`/usr/bin/docker inspect --format='{{.State.Health.Status}}' $1`
 
-if [ $STATUS != "healthy" ]
-then
-	echo restarting ...
-	/usr/bin/docker restart minecraft
-	echo done
-else
-	echo nothing to be done
-        /usr/bin/docker exec minecraft rcon-cli list
-        /usr/bin/docker exec minecraft rcon-cli banlist
-fi
+	if [ $STATUS != "healthy" ]
+	then
+		echo restarting ...
+		/usr/bin/docker restart $1
+		echo -e "done\n"
+	else
+		docker exec -i $1 rcon-cli list
+		echo -e "nothing to be done\n"
+	fi
+}
+
+for i in minecraft minecraft-small
+do
+	Health $i
+done
