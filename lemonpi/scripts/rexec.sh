b3351f86f80f6221aa0389640068b22c6c01f103
jipp <wolfgang.keller@wobilix.de>
2025-02-09 19:17:49 +0100

    added USERNAME

--- lemonpi/scripts/rexec.sh
+++ lemonpi/scripts/rexec.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+USERNAME="woke"
+
 ping -c 1 dyndns.wobilix.de > /dev/null 2>&1
 
 if [ $? -eq 0 ]
 then
 	echo logging in
-	ssh woke@dyndns.wobilix.de -p 1322 /home/$USER/docker/lemonpi/scripts/minecraft.sh
+	ssh $USERNAME@dyndns.wobilix.de -p 1322 /home/$USERNAME/docker/lemonpi/scripts/minecraft.sh
 else
 	echo server is not reachable
 fi 
