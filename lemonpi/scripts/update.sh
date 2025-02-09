3a2435a0d7bc42aa678a199068e19a101e07d177
jipp <wolfgang.keller@wobilix.de>
2025-02-09 19:19:25 +0100

    added colors and restructured

--- lemonpi/scripts/update.sh
+++ lemonpi/scripts/update.sh
@@ -1,33 +1,79 @@
-#!/bin/sh
+#!/bin/bash
 
-MCUSER=`docker exec minecraft rcon-cli list | cut -d' ' -f3`
+RESET="0"
 
-echo "\n\n-> docker: lemonpi"
-cd /home/$USER/docker/lemonpi && docker compose pull
+RED="31"
+GREEN="32"
+YELLOW="33"
+BLUE="34"
+MAGENTA="35"
 
-if [ $MCUSER -eq 0 ]
-then
-	docker compose up -d
-else
-	echo skipping minecraft
-	docker compose up -d nginx homeassistant octoprint -d
-fi
+BOLD="1"
+UNDERLINE="4"
 
-echo "\n\n-> docker: esphome"
-cd /home/$USER/docker/esphome && docker compose pull
+END="\e[${RESET}m"
+SECTION="\e[${UNDERLINE};${MAGENTA}m"
+TOPIC="\e[${UNDERLINE};${GREEN}m"
+CONTENT="\e[${BOLD};${YELLOW}m"
 
-echo "\n\n-> docker: certbot"
-cd /home/$USER/docker/certbot && docker compose pull
+User () {
+	MCUSER=`docker exec $1 rcon-cli list | cut -d' ' -f3`
 
-echo "\n\n-> docker: clean-up"
-docker system prune -f
-docker image ls
-docker ps -a
+	return $MCUSER
+}
 
+Docker () {
+	echo -e "\n\n${SECTION}docker${END}"
 
-echo "\n\n-> apt: update"
-sudo apt update && sudo apt list --upgradable && sudo apt upgrade
+	echo -e "\n-> ${TOPIC}docker:${END} ${CONTENT}lemonpi pull${END}"
+	cd /home/$USER/docker/lemonpi && docker compose pull
 
-echo "\n\n-> apt: clean-up"
-sudo apt autoremove -y
+	echo -e "\n-> ${TOPIC}docker:${END} ${CONTENT}lemonpi up${END}"
+	for i in minecraft minecraft-small
+	do
+		User $i
+		RES=$?
+		if [ $RES -eq 0 ]; then
+			docker compose up -d $i
+		fi
+	done
+	docker compose up -d nginx homeassistant octoprint minecraft-backup -d
 
+	echo -e "\n-> ${TOPIC}docker:${END} ${CONTENT}esphome pull${END}"
+	cd /home/$USER/docker/esphome && docker compose pull
+
+	echo -e "\n-> ${TOPIC}docker:${END} ${CONTENT}certbot pull${END}"
+	cd /home/$USER/docker/certbot && docker compose pull
+
+	echo -e "\n-> ${TOPIC}docker:${END} ${CONTENT}prune${END}"
+	docker system prune -f
+
+	echo -e "\n-> ${TOPIC}docker:${END} ${CONTENT}repositories${END}"
+	docker image ls
+
+	echo -e "\n-> ${TOPIC}docker:${END} ${CONTENT}status${END}"
+	docker ps -a
+}
+
+Apt () {
+	echo -e "\n\n${SECTION}apt${END}"
+
+	echo -e "\n-> ${TOPIC}apt:${END} ${CONTENT}update${END}"
+	sudo apt update && sudo apt list --upgradable && sudo apt upgrade
+
+	echo -e "\n-> ${TOPIC}apt:${END} ${CONTENT}clean-up${END}"
+	sudo apt autoremove -y
+}
+
+Health () {
+	echo -e "\n\n${SECTION}health${END}"
+
+	echo -e "\n-> ${TOPIC}health:${END} ${CONTENT}dmesg${END}"
+	dmesg -e -l emerg --level=alert,crit,err,warn,notice
+}
+
+Apt
+Docker
+Health
+
+echo -e "\n"
